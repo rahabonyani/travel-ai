85cba30cb81cb1f53dae56b1ff1bc4fc18fef656
Mehrdad Nouri <mnourib12@gmail.com>
2024-04-21 13:16:42 +0330

    Config design system

---
+++ src/components/Stepper.tsx
@@ -0,0 +1,11 @@
+interface StepperProps {
+  step: number;
+}
+
+const Stepper = ({ step }: StepperProps) => {
+  return <ol className={`flex items-center w-full bg-emerald-50`}>
+
+  </ol>;
+};
+
+export default Stepper;
---
+++ src/utils/fonts.ts
@@ -0,0 +1,9 @@
+import { Poppins } from 'next/font/google';
+
+export const poppins_init = Poppins({
+  subsets: ['latin'],
+  variable: '--font-poppins',
+  weight: ['300', '400', '500', '600'],
+});
+
+export const poppins = poppins_init.variable;
