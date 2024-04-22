fbe89d4b912930d606c6d75d95c26f0b1a29f012
rahabonyani <bonyaniiraha@gmail.com>
2024-04-22 11:35:09 +0330

    change routing of itinerary

--- src/app/itinerary/(steps)/layout.tsx
+++ src/app/itinerary/(steps)/layout.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 const ItineraryLayout = ({ children }: { children: ReactNode }) => {
-  return <div className='p-4 h-full bg-red'>{children}</div>;
+  return <div className="p-4 h-full bg-red">{children}</div>;
 };
 
 export default ItineraryLayout;
